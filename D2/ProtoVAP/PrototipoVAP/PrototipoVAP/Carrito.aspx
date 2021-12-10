3d75f8e2170dc9d06490d0cbf423c42279379dfc
Lugo12 <luisagm0507@gmail.com>
2021-12-09 20:38:50 -0600

    Response Complete

---
+++ D2/ProtoVAP/PrototipoVAP/PrototipoVAP/Carrito.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Recursos/Master Page/MasterPageCliente.Master" AutoEventWireup="true" CodeBehind="Carrito.aspx.cs" Inherits="PrototipoVAP.Carrito1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ D2/ProtoVAP/PrototipoVAP/PrototipoVAP/Recursos/js/main.js
@@ -0,0 +1,35 @@
+﻿const btn_toggle = document.getElementById('btn_toggle');
+const navbar = document.getElementById('navbar');
+const sidebar = document.getElementById('sidebar-container');
+const content = document.getElementById('content1');
+
+
+btn_toggle.onclick = () => {
+    const heightNavbar = getComputedStyle(navbar).getPropertyValue("height");
+    const heightSideBar = getComputedStyle(sidebar).getPropertyValue("height");
+    sidebar.style.marginTop = heightNavbar;
+    const heightContainer = Number(heightNavbar.slice(0, heightNavbar.length - 2)) + Number(heightSideBar.slice(0, heightSideBar.length - 2));
+    content.style.marginTop = heightContainer + "px";
+}
+
+addEventListener('resize', () => {
+    console.log('jk')
+    let wnav = getComputedStyle(navbar).getPropertyValue("width");
+    wnav = Number(wnav.slice(0, wnav.length - 2));
+
+    let hnav = getComputedStyle(sidebar).getPropertyValue("height");
+    hnav = Number(hnav.slice(0, hnav.length - 2));
+
+    if (wnav > 991.6 && hnav > 400) {
+        sidebar.style.marginTop = getComputedStyle(navbar).getPropertyValue("height");
+        content.style.marginTop = getComputedStyle(navbar).getPropertyValue("height");
+    } else if (wnav < 991.6 || hnav < 400) {
+        const hNavbar = getComputedStyle(navbar).getPropertyValue("height");
+        const hSideBar = getComputedStyle(sidebar).getPropertyValue("height");
+        sidebar.style.marginTop = hNavbar;
+        const hContainer = Number(hNavbar.slice(0, hNavbar.length - 2)) + Number(hSideBar.slice(0, hSideBar.length - 2));
+        content.style.marginTop = hContainer + "px";
+    }
+
+
+})
